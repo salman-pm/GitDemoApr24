42a7e949fe0a9811b3d60d548a28275e4f393ec1
salman-pm <pmmohamedsalman@gmail.com>
2024-04-03 15:03:12 +0530

    demo2

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am Main method");
-        System.out.println("Add 1 and 2 : " + Calculator.add(1, 2));
+        Demo2.demo();
     }
 }
