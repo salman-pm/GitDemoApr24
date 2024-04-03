d4fc59ce302332245e91d15494f930f8239ab0cc
salman-pm <pmmohamedsalman@gmail.com>
2024-04-03 14:55:34 +0530

    Added Home Loan Calculator

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am Main method");
         System.out.println("Add 1 and 2 : " + Calculator.add(1, 2));
-        System.out.println("Calculate Interest: " + InterestCalculator.calculate(1000000, 10, 8));
-        System.out.println("SIP: " + SIPCalculator.calculate(8000, 15, 18));
+        System.out.println("Home Loan: " + HomeLoanCalculator.calculate(2000000, 15, 9.0));
     }
 }
