5357e3810db80453da9e6be20cb4bf56825f8c57
salman-pm <pmmohamedsalman@gmail.com>
2024-04-03 14:29:37 +0530

    Hello World! -> I am main method

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("I am Main method");
         System.out.println("Add 1 and 2 : " + Calculator.add(1, 2));
     }
 }
