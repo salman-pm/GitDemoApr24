8a44e6881274c732a1f2f9ef8aa517c57da5dca6
salman-pm <pmmohamedsalman@gmail.com>
2024-04-03 14:44:10 +0530

    Modified formula for calculating interest

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am Main method");
         System.out.println("Add 1 and 2 : " + Calculator.add(1, 2));
+        System.out.println("Calculate Interest: " + InterestCalculator.calculate(1000000, 10, 8));
     }
 }
